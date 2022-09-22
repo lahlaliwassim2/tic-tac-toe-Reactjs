773f88cef457d92be7435e1036cb8475c4ecefd9
lahlaliwassim2 <wlahlali34@gmail.com>
2022-09-22 11:35:45 +0100

    Realiser les taches suivantes -ajouter l application react par (npx create-react-app Nameofproject -ajouter les composants avec les quelles j ai besoin -ajouter un style dans le fichiers css ²

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# tic-tac-toe-Reactjs
+réalisation de jeu tic tac toe avec react js 
---
+++ src/App.js
@@ -0,0 +1,16 @@
+import {Start} from "./components/start/Start"
+import {Board} from "./components/board/Board"
+
+
+function App() {
+  return (
+    <div className="  App">
+        <div className="container">
+             <Start />
+              <Board />
+        </div>
+  </div>
+  );
+}
+
+export default App;
