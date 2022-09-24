bd5cfda2555d5f90699c1035ea7244d0eb7443cc
lahlaliwassim2 <wlahlali34@gmail.com>
2022-09-24 12:49:24 +0100

    commenter le component de la page start pour styler l apage board

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="  App">
         <div className="container">
-             <Start />
+             {/* <Start />  */}
               <Board />
         </div>
   </div>
