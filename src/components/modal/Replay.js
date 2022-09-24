f7e58a1930d82b9f0b011679301f10fbdc9849b0
lahlaliwassim2 <wlahlali34@gmail.com>
2022-09-24 18:53:48 +0100

    ajouter le composant qui contient le modal qui perment de coisir si l utulisateur veut rejouer

---
+++ src/components/modal/Replay.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Replay() {
+  return (
+    <div className='restart'>
+        <h3 className='restart__titre'>ach khasser 3awad l3ab </h3>
+             <div className='restart_btn'>
+                <button className='btn btn-sm'> la ankhroj</button>
+                <button className='btn btn-sm btn-yellow'>  a 3awd</button>
+                </div>   
+    
+    
+    </div>
+  )
+}
