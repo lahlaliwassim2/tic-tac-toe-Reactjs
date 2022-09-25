7c0221ec95881b5672eb02fb29d1dbec641952ae
lahlaliwassim2 <wlahlali34@gmail.com>
2022-09-25 18:11:44 +0100

    ajouter le useContext 'Modalcontext' et la condition pour si la fonction show existe on affiche le modal

--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import { useContext } from 'react'
 import Replay from './Replay'
 import { Win } from './Win'
+import {ModalContext} from "../../context/ModalContext"
 export const Modal = () => {
+  const {show , showModal} = useContext(ModalContext)
   return (
+    <>
+    {show && (
     <div className='modal'>
         <div className='modal__content'>
             <div className='container'>
@@ -12,5 +17,8 @@ export const Modal = () => {
             </div>
         </div>
     </div>
+    )}
+    <button onClick={showModal}>testModal</button>
+    </>
   )
 }
